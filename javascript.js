2e2bd6a9f40afe481a70681bd9b4218f90696e9d
MalikUmair972 <mu501362@gmail.com>
2024-10-28 17:39:03 +0500

    Initial UI

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+function showModal() {
+    let modal = document.getElementById('modal-content');
+    modal.classList.add('show-content');
+}
+
+function closeModal() {
+    let modal = document.getElementById('modal-content');
+    modal.classList.remove('show-content');
+}
